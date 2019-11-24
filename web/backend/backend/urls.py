a87390c5d3b42454da2f8ff0dfe1800a5823af1f
Michael Salamakha <Wolf2195@gmail.com>
2019-11-24 20:30:06 +0300

    output django admin
    Fixed django admin output table

--- web/backend/backend/urls.py
+++ web/backend/backend/urls.py
@@ -20,6 +20,6 @@ import core.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('datalist/', include(
-        # core.urls, namespace='core')),
+    path('', include(
+        core.urls, namespace='core')),
 ]
